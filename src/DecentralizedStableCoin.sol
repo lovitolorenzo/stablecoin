3295d9b16ee23ec950697b2a9e522a303a1a97bf
Lorenzo <lovitolorenzo@yahoo.it>
2024-12-30 17:45:51 +0100

    Commit before installing OpenZeppelin contracts

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,37 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+/*
+ * @title Decentralized Stable Coin
+ * @author Lorenzo Lovito
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin {
+
+}
